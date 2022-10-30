34c7c15a64bd43532fceee6ca8b59ba385458499
AtumaDavid <atumadavid35@gmail.com>
2022-10-30 05:17:31 +0100

    modified appjs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/zuri-task-one/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
